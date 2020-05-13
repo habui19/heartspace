d93d8fd1819d44f5de78da7663691d156cfce400
Ha Bui <thuhaa196@gmail.com>
2020-05-13 17:33:41 +1000

    final v2

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let greetings = [
     'Lại đây, kể cho ba mẹ nghe xem có chuyện gì'
 ];
 
-let acceptantMessages = ['Mẹ hiểu',
+let acceptantMessages = [
     'Con có lý do chính đáng để cảm thấy như vậy',
     'Mẹ thương',
     'Mẹ luôn ở đây với con',
-    'Giờ thì hãy để những cảm xúc này đi nhé',
+    'Giờ thì con hãy để những cảm xúc này đi nhé',
     'Mẹ yêu con',
 ];
 
@@ -15,13 +15,36 @@ let learn = [
 ];
 
 let goodbye = [
-    'Con làm tốt lắm',
     'Ba mẹ rất tự hào về con',
     'Hãy vững tin bước tiếp trên hành trình của mình con nhé',
     'Nếu mệt thì về đây nghỉ với ba mẹ',
     'Ba mẹ luôn ở trong trái tim con, hỗ trợ khi con cần',
     'Ba mẹ yêu con'
 ];
+
+const text = document.querySelector(".heartspace");
+const splitText = $('.heartspace').text().split("");
+
+$('.heartspace').text("");
+for (let i = 0; i < splitText.length; i++) {
+    document.querySelector('.heartspace').innerHTML += `<span>${splitText[i]}</span>`;
+}
+
+let char = 0;
+let timer = setInterval(onTick, 100);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if (char === splitText.length) {
+        clearInterval(timer);
+        $('.description').fadeOut(500, function () {
+            $('.description').text("Về nhà với trái tim").fadeIn(1000);
+        });
+        return;
+    }
+}
 
 
 function disappear() {
@@ -36,9 +59,11 @@ function displayMessages(messages) {
     messageInterval = setInterval(function () {
         const newText = messages[i];
         i++;
+
         if (i <= messages.length) {
             $('.insideMessage').fadeOut(500, function () {
                 $('.insideMessage').text(newText).fadeIn(500);
+                console.log('working')
             });
         } else {
             clearTimeout(messageInterval);
@@ -46,44 +71,46 @@ function displayMessages(messages) {
     }, 3000);
 }
 
-function nextStep() {
-    $('.insideMessage').text(learn[0]);
-    $('textarea').val('');
-    $('.thoughts').animate({
-        opacity: '100'
-    }, 1000);
-    let remove = setTimeout(function () {
-        $('#next').remove();
-    }, 1000)
-    let newButton = "<button class='button' id='done' onclick='finalStep();'>Done</button>";
-    let nextStep = setTimeout(function () {
-        $('.page-main').append(newButton);
-    }, 15000)
+
+function enableButton(buttonId) {
+    $('button').prop('disabled', true);
+    $('button').removeClass('button');
+    $(buttonId).prop('disabled', false);
+    $(buttonId).attr('class', 'button');
 }
 
-function finalStep() {
-    displayMessages(goodbye);
-}
-
-$(window).on("load", function () {
+function startFunc() {
+    $('.welcome').addClass('hide');
+    enableButton('#letgo');
+    $('.firstStep').removeClass('hide');
     displayMessages(greetings);
     $('.thoughts').animate({
         opacity: '100'
-    }, 100000);
-})
+    }, 1000);
 
-$('textarea').on("change", function () {
     document.querySelector('.audio').play();
-})
+}
 
-$('#letgo').on("click", function () {
+function letGo() {
+    $('.insideMessage').text("Mẹ hiểu").fadeIn(600);
     displayMessages(acceptantMessages);
     disappear();
-    let remove = setTimeout(function () {
-        $('#letgo').remove();
-    }, 1000)
-    let newButton = "<button class='button' id='next' onclick='nextStep();'>Next</button>";
-    let nextStep = setTimeout(function () {
-        $('.page-main').append(newButton);
-    }, 25000)
-})
+    $('#letgo').addClass('hide');
+    let nextButton = setTimeout(function () {
+        enableButton('#next');
+    }, 17000);
+
+}
+
+function nextStep() {
+    $('.firstStep').addClass('hide');
+    $('.nextStep').removeClass('hide');
+    $('.insideMessage').text(learn[0]);
+    enableButton('#done');
+    $('.nextStep > .thoughts').css('opacity', '100');
+}
+
+function finalStep() {
+    $('.insideMessage').text("Con làm tốt lắm").fadeIn(600);
+    displayMessages(goodbye);
+}
